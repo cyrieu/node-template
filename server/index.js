c8aa275100b17f3d823db604a0ed9b8e5bd6fcd5
cyrieu <cyrieu@gmail.com>
2018-04-16 23:56:11 -0700

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => res.send("Hello world!"));
+
+app.listen(process.env.PORT, () =>
+  console.log("Listening on port: " + process.env.PORT)
+);
