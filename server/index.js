7d3ac27a446e414081d039fdd90ed726d3bfbf34
cyrieu <cyrieu@gmail.com>
2018-04-20 21:29:53 -0700

    Initial commit

--- server/index.js
+++ server/index.js
@@ -1,9 +1,46 @@
+// @flow
 import express from "express";
+import bodyParser from "body-parser";
+import { graphqlExpress, graphiqlExpress } from "apollo-server-express";
+import { makeExecutableSchema } from "graphql-tools";
 
 const app = express();
 
+// Some fake data
+const books = [
+  {
+    title: "Harry Potter and the Sorcerer's stone",
+    author: "J.K. Rowling"
+  },
+  {
+    title: "Jurassic Park",
+    author: "Michael Crichton"
+  }
+];
+
+// The GraphQL schema in string form
+const typeDefs = `
+  type Query { books: [Book] }
+  type Book { title: String, author: String }
+`;
+
+// The resolvers
+const resolvers = {
+  Query: { books: () => books }
+};
+
+// Put together a schema
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers
+});
+
 app.get("/", (req, res) => res.send("Hello world!"));
 
+// graphql
+app.use("/graphql", bodyParser.json(), graphqlExpress({ schema }));
+app.use("/graphiql", graphiqlExpress({ endpointURL: "/graphql" }));
+
 app.listen(process.env.PORT, () =>
-  console.log("Listening on port: " + process.env.PORT)
+  console.log("Listening on port: " + (process.env.PORT || "3000"))
 );
